26be9196f39870c155e5994a8ee23e1520a7c869
JORGE <jorge.mendoza@pucp.edu.pe>
2021-05-17 21:51:01 -0500

    1-Menú principal

---
+++ FBA_Solution/FBA_View/menu_principal.cpp
@@ -0,0 +1,12 @@
+#include "menu_principal.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	//LPOOInduction is your project name
+	FBAView::menu_principal form;
+	Application::Run(% form);
+}
