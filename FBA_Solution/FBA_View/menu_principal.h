521315fd80ecc2a28dae69528f365d9905c1e71f
JORGE <jorge.mendoza@pucp.edu.pe>
2021-05-18 23:03:32 -0500

    Menu principal(1)

--- FBA_Solution/FBA_View/menu_principal.h
+++ FBA_Solution/FBA_View/menu_principal.h
@@ -35,6 +35,10 @@ namespace FBAView {
 			}
 		}
 	private: System::Windows::Forms::Button^ btn_survival;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Panel^ panel1;
+
+
 	protected:
 
 	private:
@@ -50,12 +54,17 @@ namespace FBAView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(menu_principal::typeid));
 			this->btn_survival = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// btn_survival
 			// 
-			this->btn_survival->Location = System::Drawing::Point(288, 226);
+			this->btn_survival->Location = System::Drawing::Point(674, 366);
 			this->btn_survival->Name = L"btn_survival";
 			this->btn_survival->Size = System::Drawing::Size(146, 47);
 			this->btn_survival->TabIndex = 1;
@@ -63,16 +72,44 @@ namespace FBAView {
 			this->btn_survival->UseVisualStyleBackColor = true;
 			this->btn_survival->Click += gcnew System::EventHandler(this, &menu_principal::btn_survival_Click);
 			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(163, 166);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 4;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &menu_principal::pictureBox1_Click);
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(3)), static_cast<System::Int32>(static_cast<System::Byte>(37)),
+				static_cast<System::Int32>(static_cast<System::Byte>(76)));
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(249, 957);
+			this->panel1->TabIndex = 3;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &menu_principal::panel1_Paint);
+			// 
 			// menu_principal
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(775, 482);
+			this->ClientSize = System::Drawing::Size(1178, 957);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->btn_survival);
 			this->IsMdiContainer = true;
+			this->MinimumSize = System::Drawing::Size(1200, 675);
 			this->Name = L"menu_principal";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"menu_principal";
 			this->Load += gcnew System::EventHandler(this, &menu_principal::menu_principal_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
@@ -82,5 +119,9 @@ namespace FBAView {
 	private: System::Void btn_survival_Click(System::Object^ sender, System::EventArgs^ e) {
 		Game_loop::run();
 	}
-	};
+	private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
